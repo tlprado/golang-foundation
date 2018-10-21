6b2784d15183196537881c844154c2aa94d36d0e
Thiago Prado <tlprado@live.com>
2018-10-21 13:28:52 -0300

    Add files via upload
    First Commit

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os Códigos em Go são organizados em pacotes
+e para usa-lo é necessário declarar um ou varios imports
+*/
+import "fmt"
+
+//A porta de entrada para um programa Go é a função main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
